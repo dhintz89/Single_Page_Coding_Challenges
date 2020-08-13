8a8c85cebba6f4ae886ee038f1e6c19dc414a1eb
Dan Hintz <33042889+dhintz89@users.noreply.github.com>
2020-08-12 23:40:47 -0400

    Create MergingObjectArray.js
    a meeting is stored as objects ↴ with integer properties startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
    
    For example:
    
      { startTime: 2, endTime: 3 }  // meeting from 10:00 – 10:30 am
    { startTime: 6, endTime: 9 }  // meeting from 12:00 – 1:30 pm
    
    JavaScript
    Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns an array of condensed ranges.
    
    For example, given:
    
      [
      { startTime: 0,  endTime: 1 },
      { startTime: 3,  endTime: 5 },
      { startTime: 4,  endTime: 8 },
      { startTime: 10, endTime: 12 },
      { startTime: 9,  endTime: 10 },
    ]
    
    JavaScript
    your function would return:
    
      [
      { startTime: 0, endTime: 1 },
      { startTime: 3, endTime: 8 },
      { startTime: 9, endTime: 12 },
    ]
    
    JavaScript
    Do not assume the meetings are in order. The meeting times are coming from multiple teams.
    
    Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the function to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.

---
+++ MergingObjectArray.js
@@ -0,0 +1,118 @@
+function mergeRanges(meetings) {
+  
+  // Merge meetings ranges
+  
+  // sort array so that first number is always the earliest meeting start time
+  meetings.sort((a, b) => {return a.startTime - b.startTime})
+  
+  // create new array with first object as a starting point
+  const answer = [meetings[0]]
+  
+  // starting at 2nd input, compare to the most recent answer
+  for(let i=1; i<meetings.length; i++) {
+    let workingVar = answer[answer.length-1];
+    
+    // if most recent answer's end time is greater than next meeting's start time, they should be merged, use first meeting's start time and the later of the 2 end times
+    if (workingVar.endTime >= meetings[i].startTime) {
+      if (workingVar.endTime >= meetings[i].endTime) {
+        workingVar.endTime = workingVar.endTime;
+      } else {
+        workingVar.endTime = meetings[i].endTime;
+      }
+      
+    // otherwise they should be separate - so add the new meeting to the answer array and use that as the most recent answer going forward
+    } else {
+      answer.push(Object.assign({}, meetings[i]));
+    }
+  }
+  return answer;
+}
+
+
+
+
+// Tests
+
+let desc = 'meetings overlap';
+let actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]);
+let expected = [{ startTime: 1, endTime: 4 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings touch';
+actual = mergeRanges([{ startTime: 5, endTime: 6 }, { startTime: 6, endTime: 8 }]);
+expected = [{ startTime: 5, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meeting contains other meeting';
+actual = mergeRanges([{ startTime: 1, endTime: 8 }, { startTime: 2, endTime: 5 }]);
+expected = [{ startTime: 1, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings stay separate';
+actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }]);
+expected = [{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'multiple merged meetings';
+actual = mergeRanges([
+  { startTime: 1, endTime: 4 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 5, endTime: 8 },
+]);
+expected = [{ startTime: 1, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings not sorted';
+actual = mergeRanges([
+  { startTime: 5, endTime: 8 },
+  { startTime: 1, endTime: 4 },
+  { startTime: 6, endTime: 8 },
+]);
+expected = [{ startTime: 1, endTime: 4 }, { startTime: 5, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'oneLongMeetingContainsSmallerMeetings';
+actual = mergeRanges([
+  { startTime: 1, endTime: 10 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 6, endTime: 8 },
+  { startTime: 9, endTime: 10 },
+  { startTime: 10, endTime: 12 }
+]);
+expected = [
+  { startTime: 1, endTime: 12 }
+];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'sample input';
+actual = mergeRanges([
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+]);
+expected = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 },
+];
+assertArrayEquals(actual, expected, desc);
+
+function assertArrayEquals(a, b, desc) {
+  // Sort the keys in each meeting to avoid
+  // failing based on differences in key order.
+  orderedA = a.map( function(meeting) {
+    return JSON.stringify(meeting, Object.keys(meeting).sort());
+  });
+  orderedB = b.map( function(meeting) {
+    return JSON.stringify(meeting, Object.keys(meeting).sort());
+  });
+  const arrayA = JSON.stringify(orderedA);
+  const arrayB = JSON.stringify(orderedB);
+  if (arrayA !== arrayB) {
+    console.log(`${desc} ... FAIL: ${JSON.stringify(a)} != ${JSON.stringify(b)}`)
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+}
