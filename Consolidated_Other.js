d8cbc312a3f74e784be6d34b846c66cdda46bc0b
Dan Hintz <33042889+dhintz89@users.noreply.github.com>
2020-12-03 22:07:08 -0500

    Update Consolidated_Other.js
    Add another commit.

--- Consolidated_Other.js
+++ Consolidated_Other.js
@@ -1,7 +1,8 @@
 // Significant commits that didn't count because I used a non-master branch on my own project:
 
 const additions = [
-  {repo: "VisualStore", message: "change gemfile & gemfile.lock, create_users migration, and devise initializer to remove devise-jwt", link: "https://github.com/dhintz89/visual_store/commit/67c84b8b409d730f0105cd1cc0d1890dde8f306b"}
+  {repo: "VisualStore", message: "change gemfile & gemfile.lock, create_users migration, and devise initializer to remove devise-jwt", link: "https://github.com/dhintz89/visual_store/commit/67c84b8b409d730f0105cd1cc0d1890dde8f306b"},
+  {repo: "VisualStore", message: "update routes and User to remove devise-jwt related content", link: "https://github.com/dhintz89/visual_store/commit/e0624c535a4e42254b8c5a762e7c4127ea3d3387"}
 ]
 
 function postCommits() {
