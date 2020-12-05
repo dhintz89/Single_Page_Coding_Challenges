06ddb31b1abef780c8b5682534fb30dd0c866b45
Dan Hintz <33042889+dhintz89@users.noreply.github.com>
2020-12-04 19:23:29 -0500

    Update Consolidated_Other.js
    Added new commit.

--- Consolidated_Other.js
+++ Consolidated_Other.js
@@ -2,7 +2,8 @@
 
 const additions = [
   {repo: "VisualStore", message: "change gemfile & gemfile.lock, create_users migration, and devise initializer to remove devise-jwt", link: "https://github.com/dhintz89/visual_store/commit/67c84b8b409d730f0105cd1cc0d1890dde8f306b"},
-  {repo: "VisualStore", message: "update routes and User to remove devise-jwt related content", link: "https://github.com/dhintz89/visual_store/commit/e0624c535a4e42254b8c5a762e7c4127ea3d3387"}
+  {repo: "VisualStore", message: "update routes and User to remove devise-jwt related content", link: "https://github.com/dhintz89/visual_store/commit/e0624c535a4e42254b8c5a762e7c4127ea3d3387"},
+  {repo: "VisualStore", message: "remove devise-jwt code and replace with custom jwt functionality in Application Controller", link: "https://github.com/dhintz89/visual_store/commit/3ed84ba7df93df8653e836959c23b5dc3bd2e895"}
 ]
 
 function postCommits() {
