800935db5289453e2d5275296cb3c6ca0ef8c931
Dan Hintz <33042889+dhintz89@users.noreply.github.com>
2020-12-06 21:00:46 -0500

    Update Consolidated_Other.js

--- Consolidated_Other.js
+++ Consolidated_Other.js
@@ -5,7 +5,8 @@ const additions = [
   {repo: "VisualStore", message: "update routes and User to remove devise-jwt related content", link: "https://github.com/dhintz89/visual_store/commit/e0624c535a4e42254b8c5a762e7c4127ea3d3387"},
   {repo: "VisualStore", message: "remove devise-jwt code and replace with custom jwt functionality in Application Controller", link: "https://github.com/dhintz89/visual_store/commit/3ed84ba7df93df8653e836959c23b5dc3bd2e895"},
   {repo: "VisualStore", message: "remove devise-jwt code and replace with customized devise for Registration Controller", link: "https://github.com/dhintz89/visual_store/commit/295554e9943cbca9f6deffe9bc0f370c73b19da3"},
-  {repo: "VisualStore", message: "remove devise-jwt and replace with custom devise for SessionsController, add generate_jwt to User", link: "https://github.com/dhintz89/visual_store/commit/a5895ea6821d61f2dfb3b88f35bab63d62ec47fe"}
+  {repo: "VisualStore", message: "remove devise-jwt and replace with custom devise for SessionsController, add generate_jwt to User", link: "https://github.com/dhintz89/visual_store/commit/a5895ea6821d61f2dfb3b88f35bab63d62ec47fe"},
+  {repo: "VisualStore", message: "change JWT delivery method from JSON/LocalStorage to httpOnly cookie", link: "https://github.com/dhintz89/visual_store/commit/df3e8494bedcb901e96513fa2129e0779b277181"}
 ]
 
 function postCommits() {
