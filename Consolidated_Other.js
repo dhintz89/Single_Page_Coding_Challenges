fb76758973a77cdc63ab2c641c0bb789351e8278
Dan Hintz <33042889+dhintz89@users.noreply.github.com>
2020-12-06 21:14:26 -0500

    Update Consolidated_Other.js

--- Consolidated_Other.js
+++ Consolidated_Other.js
@@ -6,7 +6,8 @@ const additions = [
   {repo: "VisualStore", message: "remove devise-jwt code and replace with custom jwt functionality in Application Controller", link: "https://github.com/dhintz89/visual_store/commit/3ed84ba7df93df8653e836959c23b5dc3bd2e895"},
   {repo: "VisualStore", message: "remove devise-jwt code and replace with customized devise for Registration Controller", link: "https://github.com/dhintz89/visual_store/commit/295554e9943cbca9f6deffe9bc0f370c73b19da3"},
   {repo: "VisualStore", message: "remove devise-jwt and replace with custom devise for SessionsController, add generate_jwt to User", link: "https://github.com/dhintz89/visual_store/commit/a5895ea6821d61f2dfb3b88f35bab63d62ec47fe"},
-  {repo: "VisualStore", message: "change JWT delivery method from JSON/LocalStorage to httpOnly cookie", link: "https://github.com/dhintz89/visual_store/commit/df3e8494bedcb901e96513fa2129e0779b277181"}
+  {repo: "VisualStore", message: "change JWT delivery method from JSON/LocalStorage to httpOnly cookie", link: "https://github.com/dhintz89/visual_store/commit/df3e8494bedcb901e96513fa2129e0779b277181"},
+  {repo: "VisualStore", message: "change frontend fetch calls to include credentials", link: "https://github.com/dhintz89/visual_store/commit/b3f298316e8ebde0d4f2038ecc488553f862fbf7"}
 ]
 
 function postCommits() {
